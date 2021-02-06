c44ca14b1e583afd4ea42512d443b06942acd8d1
Pak Vladimir <vladpak555@gmail.com>
2021-02-06 11:48:15 +0500

    Changed plugins list

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,16 @@ let path = {
 	build: {
 		html: project_folder + "/",
 		css: project_folder + "/css/",
-		js: project_folder + "/js/",
-		img: project_folder + "/img/",
-		fonts: project_folder + "/fonts/",
+		js: project_folder + "/js/"
 	},
 	src: {
 		html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
 		css: source_folder + "/scss/style.scss",
-		js: source_folder + "/js/script.js",
-		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-		fonts: source_folder + "/fonts/*.ttf",
+		js: source_folder + "/js/script.js"
 	},
 	watch: {
 		html: source_folder + "/**/*.html",
-		css: source_folder + "/scss/**/*.scss",
-		js: source_folder + "/js/**/*.js",
-		img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
+		css: source_folder + "/scss/**/*.scss"
 	},
 	clean: "./" + project_folder + "/"
 }
@@ -38,7 +32,10 @@ let { src, dest } = require('gulp'),
 	group_media = require("gulp-group-css-media-queries"),
 	clean_css = require("gulp-clean-css"),
 	rename = require("gulp-rename"),
-	uglify = require("gulp-uglify-es").default,
+	uglify = require("gulp-uglify-es").default;
+
+
+
 	imagemin = require("gulp-imagemin"),
 	webphtml = require('gulp-webp-html'),
 	webp = require('imagemin-webp'),
@@ -83,12 +80,6 @@ function css() {
 				cascade: true
 			})
 		)
-		.pipe(webpcss(
-			{
-				webpClass: "._webp",
-				noWebpClass: "._no-webp"
-			}
-		))
 		.pipe(dest(path.build.css))
 		.pipe(clean_css())
 		.pipe(
@@ -114,97 +105,19 @@ function js() {
 		.pipe(dest(path.build.js))
 		.pipe(browsersync.stream())
 }
-function images() {
-	return src(path.src.img)
-		.pipe(newer(path.build.img))
-		.pipe(
-			imagemin([
-				webp({
-					quality: 75
-				})
-			])
-		)
-		.pipe(
-			rename({
-				extname: ".webp"
-			})
-		)
-		.pipe(dest(path.build.img))
-		.pipe(src(path.src.img))
-		.pipe(newer(path.build.img))
-		.pipe(
-			imagemin({
-				progressive: true,
-				svgoPlugins: [{ removeViewBox: false }],
-				interlaced: true,
-				optimizationLevel: 3 // 0 to 7
-			})
-		)
-		.pipe(dest(path.build.img))
-}
-function fonts() {
-	src(path.src.fonts)
-		.pipe(ttf2woff())
-		.pipe(dest(path.build.fonts));
-	return src(path.src.fonts)
-		.pipe(ttf2woff2())
-		.pipe(dest(path.build.fonts));
-};
-gulp.task('otf2ttf', function () {
-	return src([source_folder + '/fonts/*.otf'])
-		.pipe(fonter({
-			formats: ['ttf']
-		}))
-		.pipe(dest(source_folder + '/fonts/'));
-})
-gulp.task('svgSprite', function () {
-	return gulp.src([source_folder + '/iconsprite/*.svg'])
-		.pipe(svgSprite({
-			mode: {
-				stack: {
-					sprite: "../icons/icons.svg",  //sprite file name
-					example: true
-				}
-			},
-		}
-		))
-		.pipe(dest(path.build.img))
-})
-function fontsStyle(params) {
-	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-		return fs.readdir(path.build.fonts, function (err, items) {
-			if (items) {
-				let c_fontname;
-				for (var i = 0; i < items.length; i++) {
-					let fontname = items[i].split('.');
-					fontname = fontname[0];
-					if (c_fontname != fontname) {
-						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-					}
-					c_fontname = fontname;
-				}
-			}
-		})
-	}
-}
+
 function cb() { }
 function watchFiles(params) {
 	gulp.watch([path.watch.html], html);
 	gulp.watch([path.watch.css], css);
 	gulp.watch([path.watch.js], js);
-	gulp.watch([path.watch.img], images);
 }
 function clean(params) {
 	return del(path.clean);
 }
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+let build = gulp.series(clean, gulp.parallel(js, css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-exports.fontsStyle = fontsStyle;
-exports.fonts = fonts;
-exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
